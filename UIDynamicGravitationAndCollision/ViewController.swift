25eb66837d76d6d6e3b30c697059d35c40a07bcd
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-04-24 19:35:25 +0300

    Initial Commit

---
+++ UIDynamicGravitationAndCollision/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIDynamicGravitationAndCollision
+//
+//  Created by Артем on 4/24/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
